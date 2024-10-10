61b5c1a1282ee134f3c994145718e050f3ac3e22
ninahand <ninahand@newschool.edu>
2024-10-09 20:53:56 -0400

    push 2

--- pg1/script.js
+++ pg1/script.js
@@ -4,47 +4,69 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             const images = data.map(item => item.image);
             const quotes = data.map(item => item.quote || "");
+            const medals = data.map(item => item.medal || "");
 
             const imageElement = document.querySelector(".image");
             const quoteElement = document.querySelector(".quote");
+            const medalElement = document.querySelector(".medal-container"); 
 
             let currentIndex = 0;
             let isScrolling = false;
 
             function updateImageAndQuote(index) {
-                const adjustedIndex = index % images.length; // Ensure index wraps around
+                const adjustedIndex = index % images.length; 
                 imageElement.src = images[adjustedIndex];
                 quoteElement.textContent = quotes[adjustedIndex];
+                medalElement.textContent = medals[adjustedIndex]; // Update medal content
             }
 
-            // Initial setting
             updateImageAndQuote(currentIndex);
 
             window.addEventListener("wheel", function(event) {
-                if (isScrolling) return; // Prevents rapid firing of scroll events
+                if (isScrolling) return; 
 
                 isScrolling = true;
 
                 if (event.deltaY > 0) {
-                    // Scroll down
                     currentIndex = (currentIndex + 1) % images.length;
                 } else {
-                    // Scroll up
                     currentIndex = (currentIndex - 1 + images.length) % images.length;
                 }
 
                 updateImageAndQuote(currentIndex);
 
-                // Throttle the scroll event for 300ms
                 setTimeout(function() {
                     isScrolling = false;
-                }, 200);
+                }, 1);
             });
         })
         .catch(error => console.error("Error loading images and quotes from JSON:", error));
 });
 
+document.addEventListener('wheel', function() {
+    function randomNote() {
+        const notes = ["C2", "D2", "E2", "F2", "G2", "A2", "B2"]; 
+        return notes[Math.floor(Math.random() * notes.length)];
+    }
 
+    function newSynth(){
+        const synth = new Tone.Synth({
+            oscillator:{
+                type: 'triangle'  
+            }, 
+            envelope:{
+                attack: 2,      
+                decay: 0.5,
+                sustain: 0.7,   
+                release: 3      
+            }
+        }).toDestination();
+    
+        synth.volume.value = -15;  
+        synth.triggerAttackRelease(randomNote(), "8"); 
+    }
+    newSynth();
+});
 
 // var winHeight = $(window).innerHeight();
 // $(document).ready(function () {
